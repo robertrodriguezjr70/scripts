9b6fdac5ea8902b459d32b9a37cfbbf8f0011d4a
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2026-03-24 09:44:55 -0700

    Delete InstallDockContainerStoppedAlertScipt.sh

--- InstallDockContainerStoppedAlertScipt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /root || exit 1
-curl -fL -O https://raw.githubusercontent.com/robertrodriguezjr70/scripts/main/DockerContainerDownCheck.sh
-chmod +x /root/DockerContainerDownCheck.sh
-(crontab -l 2>/dev/null; echo '* * * * * /bin/bash /root/DockerContainerDownCheck.sh') | crontab -
