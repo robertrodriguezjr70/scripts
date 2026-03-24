479810f7b65a8ced1d04f632fa2e255d88b8ad44
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2026-03-24 06:17:11 -0700

    Refactor InstallDockContainerStoppedAlertScript.sh
    Removed unnecessary commands and streamlined the script.

--- InstallDockContainerStoppedAlertScipt.sh
+++ InstallDockContainerStoppedAlertScipt.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# become root
-sudo su 
-cd ~
-
-# Pull script from repo and make it executable
+cd /root || exit 1
 curl -fL -O https://raw.githubusercontent.com/robertrodriguezjr70/scripts/main/DockerContainerDownCheck.sh
-
-chmod +x DockerContainerDownCheck.sh
-
-#Modify crontab
+chmod +x /root/DockerContainerDownCheck.sh
 (crontab -l 2>/dev/null; echo '* * * * * /bin/bash /root/DockerContainerDownCheck.sh') | crontab -
-
