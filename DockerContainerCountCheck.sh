e9920fb92a63980b09a8cc59cf5ba217ac70bec4
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2026-04-01 09:48:52 -0700

    Add Docker container count check script
    This script checks the number of running Docker containers and writes the count to a file. It can be scheduled to run periodically using cron.

---
+++ DockerContainerCountCheck.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# add this to crontab -e so that script runs every 60 seconds
+# */10 * * * * /root/DockerContainerDownCheck.sh
+
+set -euo pipefail
+
+FILE="dockerContainerCount.txt"
+NAMESPACE="Custom/Docker"
+METRIC_NAME="ContainerDown"
+REGION="eu-west-1"
+
+RUNNING_COUNT="$(docker info 2>/dev/null | awk -F': ' '/Running:/ {print $2}' | xargs)"
+
+VALUE=1
+if [[ "$RUNNING_COUNT" =~ ^[0-9]+$ ]] && [ "$RUNNING_COUNT" -gt 1 ]; then
+  VALUE="$RUNNING_COUNT"
+fi
+
+echo "$VALUE" > "$FILE"
+
+
+#TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+#  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+#INSTANCE_ID=$(curl -s \
+#  -H "X-aws-ec2-metadata-token: $TOKEN" \
+#  http://169.254.169.254/latest/meta-data/instance-id)
+
+
+#aws cloudwatch put-metric-data \
+#  --namespace "$NAMESPACE" \
+#  --metric-name "$METRIC_NAME" \
+#  --value "$VALUE" \
+#  --unit Count \
+#  --region "$REGION" \
+#  --dimensions InstanceId="$INSTANCE_ID"
+
+
+
+#echo $NAMESPACE
+#echo $METRIC_NAME
+#echo $VALUE
+#echo $REGION
+#echo $INSTANCE_ID
