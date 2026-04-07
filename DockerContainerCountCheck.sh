3441462073aaf19263f9d386a152ecc597b1e88d
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2026-04-07 05:10:59 -0700

    Change metric name from ContainerDown to ContainerUp

--- DockerContainerCountCheck.sh
+++ DockerContainerCountCheck.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 04/01/2026 rrodriguez - This script will check docker info to see how many containers are running, and push the container count to a file named dockerContainerCount.txt
-#                         It will then push metrics to AWS Cloudwatch. Cloudwatch will then check to see if the container count > 1 and alert if more than one containers is running.
 # add this to crontab -e so that script runs every 60 seconds
 # */10 * * * * /root/DockerContainerDownCheck.sh
 
@@ -9,7 +7,7 @@ set -euo pipefail
 
 FILE="dockerContainerCount.txt"
 NAMESPACE="Custom/Docker"
-METRIC_NAME="ContainerDown"
+METRIC_NAME="ContainerUp"
 REGION="eu-west-1"
 
 RUNNING_COUNT="$(docker info 2>/dev/null | awk -F': ' '/Running:/ {print $2}' | xargs)"
@@ -22,21 +20,21 @@ fi
 echo "$VALUE" > "$FILE"
 
 
-#TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
-#  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
-#INSTANCE_ID=$(curl -s \
-#  -H "X-aws-ec2-metadata-token: $TOKEN" \
-#  http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCE_ID=$(curl -s \
+  -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/instance-id)
 
 
-#aws cloudwatch put-metric-data \
-#  --namespace "$NAMESPACE" \
-#  --metric-name "$METRIC_NAME" \
-#  --value "$VALUE" \
-#  --unit Count \
-#  --region "$REGION" \
-#  --dimensions InstanceId="$INSTANCE_ID"
+aws cloudwatch put-metric-data \
+  --namespace "$NAMESPACE" \
+  --metric-name "$METRIC_NAME" \
+  --value "$VALUE" \
+  --unit Count \
+  --region "$REGION" \
+  --dimensions InstanceId="$INSTANCE_ID"
 
 
 
